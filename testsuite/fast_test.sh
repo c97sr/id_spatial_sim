6614d69eb3cbc991906177b70c405529430d533d
c97sr <sr@stevenriley.net>
2014-10-08 15:56:36 +0100

    Quite a bit on the test suite

--- testsuite/fast_test.sh
+++ testsuite/fast_test.sh
@@ -4,5 +4,8 @@ BINDIR="../g++/"
 BUILD="ebola_build.exe"
 RUN="ebola_run.exe"
 
-$BINDIR$BUILD ./pop1_params.in ./output/pop1
-$BINDIR$RUN ./output/pop1 ./output/pop1_test1 
+# rm ./output/*
+# rm ./figs/*
+# $BINDIR$BUILD ./params/fast_test_build_params.in ./output/small_pop1
+$BINDIR$RUN ./params/fast_test_run_params.in  ./output/small_pop1 ./output/ft_sp
+# Rscript -e "require(knitr); knit2pdf('fast_test.Rnw')"
