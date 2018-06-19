b9884ef75e8955de4fc4c66f3f90e62481e1bf85
Steven <Steven@WPIA-DIDE241.lan>
2018-06-19 14:31:40 +0100

    Started adding the gpl notice.

--- src/cpp/SR_BytePackedInt.cpp
+++ src/cpp/SR_BytePackedInt.cpp
@@ -1,3 +1,21 @@
+/*  Copyright 2018 Steven Riley.
+
+    This file is part of id_spatial_sim.
+
+    id_spatial_sim is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    id_spatial_sim is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with id_spatial_sim.  If not, see <https://www.gnu.org/licenses/>.  */
+
+
 #include"SR_BytePackedInt.h"
 #include<limits>
 
