c031125b86b70b0a8c7fcab4856f38675bdc7039
sriley <sriley@WPIA-DIDELT228>
2018-07-23 15:45:50 +0100

    Adding in the midwest influenza scenario

---
+++ scenarios/midwest_flu/ebola_run_on.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TOP="../../"
+BINDIR=$TOP"g++/"
+PARSTEM="both"
+BUILD="ebola_build.exe"
+RUN="ebola_run.exe"
+
+OUTDIR="./output/"
+NETDIR="./network/"
+OUTSTEM="ebola_pop"
+
+$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola
+
+
+
+
+
---
+++ scenarios/midwest_flu/mwf_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TOP="../../"
+BINDIR=$TOP"build/"
+PARSTEM="both"
+BUILD="ebola_build.exe"
+RUN="ebola_run.exe"
+
+OUTDIR="./output/"
+NETDIR="./network/"
+OUTSTEM="ebola_pop"
+
+$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola
+
+
