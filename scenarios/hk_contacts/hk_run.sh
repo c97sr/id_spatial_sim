08e15a8f81a0707ef1d852f918b537a2fbb9e9c4
kkokwok <kkokwok@gmail.com>
2019-07-23 12:22:02 +0100

    Update on 23th July

--- scenarios/hk_contacts/hk_run.sh
+++ scenarios/hk_contacts/hk_run.sh
@@ -9,10 +9,20 @@ RUN="ebola_run.exe"
 OUTDIR="./output/"
 NETDIR="./network/"
 OUTSTEM="hkc_build_pop"
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1x
+
+$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1a Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Vaccine_Probability_Susceptible 1  1
+$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1b Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Vaccine_Probability_Susceptible 1  0
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1a Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 10 Vaccine_Probability_Susceptible 1  1
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1b Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 10 Vaccine_Probability_Susceptible 1  0
+
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1b Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 0.5 Probability_Trace_Neighbour  1 0.5 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 999 Vaccine_Probability_Susceptible 1  0
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1b Maximum_Number_CT_Vaccinations_Each_Day 1 200 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 30 Vaccine_Probability_Susceptible 1  1
+
 #$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1a Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  999  Start_Time_Self_Isolation 1 999 Vaccine_Probability_Susceptible 1  0
 #$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1b Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  999  Start_Time_Self_Isolation 1 999 Vaccine_Probability_Susceptible 1  1
-$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1c Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 1 Vaccine_Probability_Susceptible 1  0
-$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1d Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 1 Vaccine_Probability_Susceptible 1  1
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1c Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 1 Vaccine_Probability_Susceptible 1  0
+#$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_1d Maximum_Number_CT_Vaccinations_Each_Day 1 20000 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  1  Start_Time_Self_Isolation 1 1 Vaccine_Probability_Susceptible 1  1
 #$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_2a Maximum_Number_CT_Vaccinations_Each_Day 1 30 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  999  Start_Time_Self_Isolation 1 999 Vaccine_Probability_Susceptible 1  1
 #$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_2b Maximum_Number_CT_Vaccinations_Each_Day 1 30 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  10  Start_Time_Self_Isolation 1 999 Vaccine_Probability_Susceptible 1  1
 #$BINDIR$RUN both_run.in $NETDIR${OUTSTEM} ${OUTDIR}ebola_2c Maximum_Number_CT_Vaccinations_Each_Day 1 30 Probability_Trace_Household 1 1 Probability_Trace_Neighbour  1 1 Start_Time_Contact_Tracing 1  15  Start_Time_Self_Isolation 1 999 Vaccine_Probability_Susceptible 1  1
